d8013cb603eade55b2fc885fa2357372b6567227
lauren.auchter <lauren.auchter@learnplatform.com>
2021-03-22 13:04:17 -0400

    PG ssl and formatting

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
 const knex = require("knex");
 const app = require("./app");
 const {
-  PORT,
-  DATABASE_URL_1,
-  DATABASE_URL_2,
-  DATABASE_URL_3
+	PORT,
+	DATABASE_URL_1,
+	DATABASE_URL_2,
+	DATABASE_URL_3,
 } = require("./config");
+const pg = require("pg");
+pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 const db1 = knex({
-  client: "pg",
-  connection: DATABASE_URL_1
+	client: "pg",
+	connection: DATABASE_URL_1,
 });
 
 const db2 = knex({
-  client: "pg",
-  connection: DATABASE_URL_2
+	client: "pg",
+	connection: DATABASE_URL_2,
 });
 
 const db3 = knex({
-  client: "pg",
-  connection: DATABASE_URL_3
+	client: "pg",
+	connection: DATABASE_URL_3,
 });
 
 app.set("db1", db1);
@@ -27,5 +29,5 @@ app.set("db2", db2);
 app.set("db3", db3);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+	console.log(`Server listening at http://localhost:${PORT}`);
 });
